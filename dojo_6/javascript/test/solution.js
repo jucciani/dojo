55912984af75557a8241c1f2f4c3c876cbe0dae2
jucciani <juan.ucciani@mercadolibre.com>
2017-06-08 13:35:04 -0300

    dojo6

--- dojo_6/javascript/test/solution.js
+++ dojo_6/javascript/test/solution.js
@@ -1,8 +1,19 @@
 const chai = require('chai');
+var Product = require('../src/Product');
+var Cost = require('../src/Cost');
+var UnitSale = require('../src/UnitSale');
+var SellingPrice = require('../src/SellingPrice');
 
 describe("checkout", () => {
     describe("dummy", function() {
         it("dummy", () => {
+        	var product = new Product(new Cost(10));
+        	console.log(product.cost());
+        	var sale = new UnitSale(product, new SellingPrice(20));
+        	console.log(sale);
+        	var profit = sale.profit();
+
+        	chai.assert.equal(profit, 10);
         });
     });
 });
